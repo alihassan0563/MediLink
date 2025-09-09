937798564b3d01a15f34068a919c783a882be7fb
Rebel <rebelwear40@gmail.com>
2025-09-09 10:58:05 +0500

    Contact image is updated

--- pharmacy-frontend/src/pages/Contact.jsx
+++ pharmacy-frontend/src/pages/Contact.jsx
@@ -111,7 +111,7 @@ const Contact = () => {
           <div className="contact-hero-left">
             <div className="contact-hero-image">
               <img 
-                src="https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                src="/contact image.jpg" 
                 alt="Professional customer service team" 
               />
             </div>
