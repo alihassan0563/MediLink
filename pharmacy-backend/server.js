49ebf8b4f9e27982c8a5f38bd9db5ed4d3bd54b5
AliHassanFabulous <alihassanfabulous@fabtechsol.com>
2026-04-17 00:26:04 +0500

    fixes

--- pharmacy-backend/server.js
+++ pharmacy-backend/server.js
@@ -31,11 +31,10 @@ app.use("/api/pharmacy", authLimiter, pharmacyAuthRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/admin", adminRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
-  .connect("mongodb://localhost:27017/pharmacydb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => {
